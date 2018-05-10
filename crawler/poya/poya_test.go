672031397958a841d731a46dd9c1c7cc558b24a8
hsiaoairplane <hsiaoairplane@gmail.com>
2018-05-10 22:20:56 +0800

    Update test cases.
    Signed-off-by: hsiaoairplane <hsiaoairplane@gmail.com>

--- crawler/poya/poya_test.go
+++ crawler/poya/poya_test.go
@@ -2,8 +2,11 @@ package poya
 
 import (
 	"testing"
+	"time"
 )
 
+const defaultTimeout = 30 // seconds
+
 func TestPoya(t *testing.T) {
-	Crawler("洗髮精")
+	Crawler("洗髮精", time.Duration(defaultTimeout)*time.Second)
 }
--- crawler/watsons/watsons_test.go
+++ crawler/watsons/watsons_test.go
@@ -2,8 +2,11 @@ package watsons
 
 import (
 	"testing"
+	"time"
 )
 
+const defaultTimeout = 30 // seconds
+
 func TestWatsons(t *testing.T) {
-	Crawler("洗髮精")
+	Crawler("洗髮精", time.Duration(defaultTimeout)*time.Second)
 }
